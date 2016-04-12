63a3c8ac6bf8450b77c43f690e0b01f60a278d8d
Daniel <happy.future@gmail.com>
2016-04-12 21:32:23 +0800

    publish book

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-git add * && git commit -a  -m  'publish book' && git push origin master
+git add * && git commit -a  -m  'publish book' && git push origin master  && sleep 10 && open https://dearhwj.gitbooks.io/itbook/content/index.html
